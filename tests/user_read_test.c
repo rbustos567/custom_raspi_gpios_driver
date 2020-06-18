2ded3a00fb28d86d90ab1a877a70b3b17f524224
admin <rbo567@gmail.com>
2020-06-17 21:43:33 -0500

    Adding GNU Public license to C files

--- tests/user_read_test.c
+++ tests/user_read_test.c
@@ -1,3 +1,23 @@
+/*
+ * user_read_test.c:
+ *      Copyright (c) 2020 Ricardo Bustos
+ ***********************************************************************
+ *
+ *    Customized raspi GPIOS driver for the raspberry pi 3+ is a free
+ *    software: you can redistribute it and/or modify it under the terms
+ *    of the GNU Lesser General Public License as published by the Free
+ *    Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Customized raspi GPIOS driver for the raspberry pi 3+ is distributed
+ *    in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *    without even the implied warranty of MERCHANTABILITY or FITNESS
+ *    FOR A PARTICULAR PURPOSE.
+ *    See the GNU Lesser General Public License for more details.
+ *
+ ***********************************************************************
+ */
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
